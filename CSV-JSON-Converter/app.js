640d9be135a2f570901387ae6b869deed5742fd8
Leon Schwedler <leon@EO-LA-021>
2023-08-25 13:55:22 +0200

    CSV-JSON-Converter w.i.p.

---
+++ CSV-JSON-Converter/app.js
@@ -0,0 +1,8 @@
+const inputTextElement = document.getElementById("inputText");
+const outputTextElement = document.getElementById("outputText");
+
+
+const splitInVal = inputTextElement.value.split("");
+
+console.log(splitInVal)
+
