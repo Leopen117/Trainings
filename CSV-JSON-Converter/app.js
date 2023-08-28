ba351bb6392517ba1026c8e954f0f0ea48f6f395
Leon Schwedler <leon@EO-LA-021>
2023-08-28 16:32:04 +0200

    CSV-JSON Converter wip

--- CSV-JSON-Converter/app.js
+++ CSV-JSON-Converter/app.js
@@ -1,8 +1,69 @@
 const inputTextElement = document.getElementById("inputText");
 const outputTextElement = document.getElementById("outputText");
+const btnJsonToCsv = document.querySelector("#convertToCss");
+const btnCsvToJson = document.querySelector("#convertToJson");
 
 
-const splitInVal = inputTextElement.value.split("");
+ 
+// JSON zu CSV
 
-console.log(splitInVal)
+const inputJsonObj = JSON.parse(inputTextElement.value);
 
+let result = "";
+
+btnJsonToCsv.addEventListener("click", outputCsv);
+function outputCsv() {
+    let currentLine = "";
+
+    inputJsonObj.forEach((obj)=>{
+        const inputJsonObjEntries = Object.entries(obj)
+        inputJsonObjEntries.forEach(outputCurrentLine)
+        function outputCurrentLine(entry)  {
+            const outputValue = `"${entry[1]}";`;
+            currentLine = currentLine + outputValue;
+        }  
+        currentLine = currentLine + "\n"
+    })  
+    result = result + currentLine;
+    outputTextElement.value = result
+}
+
+// CSV zu JSON
+
+btnCsvToJson.addEventListener("click", outputJson);
+
+function outputJson() {
+   
+    const splitCsvElement = inputTextElement.value.split(";");
+   
+    splitCsvElement.forEach((obj) => {
+        console.log(obj);
+    })
+    
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const obj1 = {
+//     name: "Paul",
+//     age: 28
+// }
+
+// const age = obj1.age
+
+// const str = '{name: "Paul", age: 28}'
+
+// const obj2 = JSON.parse(str)
+
+// obj2.age
