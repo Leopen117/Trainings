dba987f776e93d4bbeed3ef62611d4f2f934b349
Leon Schwedler <leon@EO-LA-021>
2023-08-31 14:52:16 +0200

    CSV-JSON-Converter wip

--- CSV-JSON-Converter/app.js
+++ CSV-JSON-Converter/app.js
@@ -2,28 +2,45 @@ const inputTxt = document.getElementById("inputText");
 const outputTxt = document.getElementById("outputText");
 const btnToCsv = document.querySelector("#convertToCsv");
 const btnToJson = document.querySelector("#convertToJson");
-const radio = (",", ";", "tab");
+const sepSelection = document.querySelectorAll("input[name='radio']");
+let currentLine = "" 
+let result = "";
 
 
 
- 
+
+
 // JSON zu CSV
 
 
-let result = "";
 
 btnToCsv.addEventListener("click", outputCsv);
 function outputCsv() {
+    sepSelection.forEach((r) => {
+        if(r.checked){
+             splitChar = r.value
+        }
+    })
     const inputJsonObj = JSON.parse(inputTxt.value);
-    let currentLine = "";
-
+    
+    
+    const inputKeys = Object.keys(inputJsonObj[0])  
+    inputKeys.forEach(header)
+    function header(key) {
+        const keysValue = `${key}${splitChar}`
+        currentLine = currentLine + keysValue
+    }   
+    console.log('HEADER currentLine :', Object.keys(inputJsonObj[0]));
+    
+    
+    currentLine = currentLine + "\n"    
+    
     inputJsonObj.forEach((obj)=>{
         const inputJsonObjEntries = Object.entries(obj)
         inputJsonObjEntries.forEach(outputCurrentLine)
-        function outputCurrentLine(entry)  {
-           // header hinzufügen
-            const outputValue = `"${entry[1]}";`;
-            currentLine = currentLine + outputValue;
+        function outputCurrentLine(entry)  {    
+            const outputValue = `${entry[1]}${splitChar}`;           
+            currentLine = currentLine + outputValue;          
         }  
         currentLine = currentLine + "\n"
     })  
@@ -34,23 +51,23 @@ function outputCsv() {
 // CSV zu JSON
 
 
-btnToJson.addEventListener("click", outputJson);
-    // Auswahl zwischen (,) oder (;)
+btnToJson.addEventListener("click", outputJson)
 function outputJson() {
+
     const splitTxt = inputTxt.value.split("\n");
     const headTxt = splitTxt.shift()
     const splitHeadTxt = headTxt.split(","); 
-    console.log("splitTxt", splitTxt);
-    console.log("headTxt", headTxt);
-    console.log("splitHeadTxt", splitHeadTxt);
-
-
- 
+    
+    sepSelection.forEach((r) => {
+        if(r.checked){
+             splitChar = r.value
+        }
+    })
     let result = []
     splitTxt.forEach((obj) => {
         const currentLine = obj.split(splitChar);
         const outputJSON = {} 
-        console.log("currentLine", currentLine)
+        
         currentLine.forEach((obj, i) => {
                 outputJSON[splitHeadTxt[i]] = currentLine[i];
                 
@@ -62,8 +79,11 @@ function outputJson() {
     outputTxt.value = JSON.stringify(result, null, 2);
 
     console.log("result", result)
-    
+     
         
+            
+
+
         
     
 
