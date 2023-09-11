6ecbc6999f0878afd19b99ce00e4e6cdba056f25
Leon Schwedler <leon@EO-LA-021>
2023-09-11 17:29:20 +0200

    Calculator wip

--- Calculator/app.js
+++ Calculator/app.js
@@ -4,25 +4,31 @@ const positiveNegative = document.getElementById("positiveNegative");
 const result = document.getElementById("result")
 const operators = document.getElementsByName("operator");
 const digits = document.getElementsByName("digit");
+const plusMinus = document.getElementById("plusMinus")
 let usedOpe = "";
 let firstNum = "";
-let secNum = "" ;
+let secNum = "";
 
 // Zahlen anzeigen
 digits.forEach((obj) => {
     obj.addEventListener("click", addDigit);
-    function addDigit() {        
+    function addDigit() {
         display.value = display.value + obj.innerHTML;
     }
 })
+// Vorzeichen umdrehen
+plusMinus.addEventListener("click", changeSign);
+function changeSign() {
+    display.value = display.value * -1;
+}
 // RechenOperation
-operators.forEach((ope) =>{
+operators.forEach((ope) => {
     ope.addEventListener("click", calculateOperation);
     function calculateOperation() {
         usedOpe = ope.innerHTML;
         firstNum = parseFloat(display.value);
         display.value = "";
-        }
+    }
 })
 // Result
 result.addEventListener("click", addResult);
