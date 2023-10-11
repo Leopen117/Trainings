fbd8d26be03f7a7358b137332636a7d4a0e8f00e
Leon Schwedler <leon@EO-LA-021>
2023-10-11 13:59:17 +0200

    Mortage Calculator

--- labor.java
+++ labor.java
@@ -4,17 +4,11 @@ public class labor {
     
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-        System.out.print("Number: ");
-        int number = scanner.nextInt();
-        if (number % 3 == 0 && number % 5 == 0) {
-            System.out.println("FizzBuzz");
-        } else if (number % 3 == 0) {
-            System.out.println("Buzz");
-        } else if (number % 5 == 0) {
-            System.out.println("Fizz");
-        } else {
-            System.out.println(number);
+        
+        String input = "";
+        while (!input.equals("quit")) {
+            System.out.print("Input: ");
+            input = scanner.next().toLowerCase();
+            System.out.println(input);
         }
-    
-}}
+    }}
